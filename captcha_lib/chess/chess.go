aef649e5e49bd3995f959eaa5a68610babf8ce94
Sad-0w <shs.shadowkitty@gmail.com>
2024-05-03 17:13:30 -0400

    added comments to chess

--- captcha_lib/chess/chess.go
+++ captcha_lib/chess/chess.go
@@ -14,11 +14,23 @@ import (
 	"github.com/notnil/chess/uci"
 )
 
+// this is the cutoff point for a chess puzzle point
+// it is measured in centipawns or 1/100 of a pawn (so 700 is 7 pawns)
+// this variable can be changed to alter the difficulty of the chess puzzles
 const CUTOFF = 700
+
+// this is the run time the engine uses to perform a estimation of the position, it should be low
+// currently it is 10 ms
 const engRuntime = time.Second / 100
+
+// this is the run time to calculate the solution to the puzzle, this will give the *best* move in the position
 const solutionTime = time.Second * 10
+
+// this is the number of puzzles to have the user solve
+// increasing this will cause the computation to increase greatly
 const PuzzleKeyLen = 2
 
+// this function handles having the user find the solution to a chess puzzle
 func promptUserInput(game *chess.Game, solution *chess.Move, retrieveKey bool) bool {
 	if retrieveKey {
 		for {
@@ -93,6 +105,7 @@ func GetPuzzleKey(pwd string, offsets []int) (string, []int) {
 	return getChessPuzzles(bpwd, engRuntime, offsets)
 }
 
+// function that takes in the byte string password, a timescale for estimations, and a skipped array (for recovering the key from the byte string)
 func getChessPuzzles(pwd []byte, timeScale time.Duration, skip []int) (string, []int) {
 	// set up engine to use stockfish exe
 	salt := make([]byte, 16)
@@ -111,17 +124,20 @@ func getChessPuzzles(pwd []byte, timeScale time.Duration, skip []int) (string, [
 	Srand := rand.New(rand.NewSource(int64(binary.BigEndian.Uint64(key))))
 
 	var result string
+	// maintain state of the number of skipped puzzles for each index
 	skipped := make([]int, PuzzleKeyLen)
 	i := 0
 
-	// randomly perform moves until the game is decided
 	for i < PuzzleKeyLen {
 		game := chess.NewGame()
+		// randomly perform moves until the game is decided or the puzzle key is finished
 		for game.Outcome() == chess.NoOutcome {
 			// select a random move
 			moves := game.ValidMoves()
 			move := moves[Srand.Intn(len(moves))]
 			game.Move(move)
+
+			// have the engine perform a quick evaluation to see if anything interesting is happening
 			cmdPos := uci.CmdPosition{Position: game.Position()}
 			cmdGo := uci.CmdGo{MoveTime: timeScale}
 			if err := eng.Run(cmdPos, cmdGo); err != nil {
@@ -129,12 +145,14 @@ func getChessPuzzles(pwd []byte, timeScale time.Duration, skip []int) (string, [
 			}
 			stat := eng.SearchResults()
 
+			// compare the current state with the cutoff point for a "puzzle point"
 			if math.Abs(float64(stat.Info.Score.CP)) > CUTOFF && i < PuzzleKeyLen {
 				if skip != nil && skip[i] > 0 {
 					skip[i]--
 					continue
 				}
 
+				// have the engine evaluate the best move in the position
 				cmdPos := uci.CmdPosition{Position: game.Position()}
 				cmdGo := uci.CmdGo{MoveTime: solutionTime}
 				if err := eng.Run(cmdPos, cmdGo); err != nil {
@@ -143,8 +161,9 @@ func getChessPuzzles(pwd []byte, timeScale time.Duration, skip []int) (string, [
 				solution_move := stat.BestMove
 
 				// this next line is for testing purposes as it will display the solution
-				fmt.Println("Best move: ", solution_move)
+				// fmt.Println("Best move: ", solution_move)
 
+				// prompt the user to find the best move
 				guess := promptUserInput(game, solution_move, skip == nil)
 				if guess {
 					result += solution_move.String()
